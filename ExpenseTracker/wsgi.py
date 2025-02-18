4afb841be98f792c295576a4ff206698caf389de
Darpit Parikh <155951559+DarpitParikh@users.noreply.github.com>
2025-02-18 17:59:20 +0530

    Add files via upload

---
+++ ExpenseTracker/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for ExpenseTracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
+
