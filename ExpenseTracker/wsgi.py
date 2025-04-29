f5802f368eae714888723f093a584f3299fad4cc
Darpit Parikh <155951559+DarpitParikh@users.noreply.github.com>
2025-04-29 22:50:58 +0530

    Update wsgi.py
    collect static

--- ExpenseTracker/wsgi.py
+++ ExpenseTracker/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExpenseTracker.settings')  # Add this line
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExpenseTracker.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'))
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
